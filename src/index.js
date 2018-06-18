64e2d8c6400be82c9e75afc4e2731157a32c4920
Kenneth Korcal <kakorcal@gmail.com>
2018-06-17 23:51:03 -0700

    adding redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import reducer from './reducers';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middleware = [thunk];
+const enhancers = [];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialState = {
+  todos: [],
+  loading: false
+};
+
+const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(...middleware), ...enhancers));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
